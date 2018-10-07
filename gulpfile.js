a3ac380317c02df4aa9732cc8666a1db352cb613
SLAYER <slayer.rus@gmail.com>
2018-10-07 21:39:36 +0300

    gulp function

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,27 @@ gulp.task('livereload', function (){
   gulp.src('./static/**/*')
     .pipe(connect.reload());
 });
+
+const colors = new Map([
+  ["white", "#fff"],
+  ["black", "#000"],
+  ["textfield-container", "#f2f2f2"],
+  ["textfield-hover", "#ebebeb"],
+  ["bg-white", "#eee"],
+  ["blue", "#81b3d2"],
+  ["blue-hover", "#669ec0"],
+  ["blue-active", "#5496bd"],
+  ["brown", "#766357"],
+  ["brown-hover", "#604e43"],
+  ["brown-active", "#503e33"],
+  ["footer-white", "#f9fbf6"],
+  ["gray", "#333"],
+  ["shadow-cont", "#000101"],
+  ["nav-current", "#766357"]
+]);
+
+
+
 //не работают кастомные функции, завести или забить...
 gulp.task('scss', () => {
   const postcss = require('gulp-postcss');
@@ -27,14 +48,24 @@ gulp.task('scss', () => {
   const precss = require('precss');
   const autoprefixer = require('autoprefixer');
   const cssnano = require('cssnano');
+  const functions = require('postcss-functions');
   return gulp
     .src('./sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(postcss([
       postcssImport(),
       precss(),
+      functions({
+        functions: {
+          color: function(key) {
+            if (){
+              return map-get($colors, $key);
+            }
+          }
+        }
+      }),
       autoprefixer({browsers: ['last 2 versions', 'IE >= 11', '> 1%']}),
-      cssnano(),
+      cssnano()
     ]))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('static'));
