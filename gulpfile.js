c5cd469e4e829f54b85eefa1b2e3464678487238
slayer.rus@gmail.com <7^0!CFc*o98ZD>
2018-10-08 16:08:03 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,12 @@ gulp.task('connect', function(){
     root: 'static',
     port: 8080,
     debug: true,
-    livereload: true,
+    livereload: true
   });
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./sass/**/*.scss', ['scss']);
+  gulp.watch('./sass/**/*.scss', ['sass']);
   gulp.watch('./static/**/*', ['livereload']);
 });
 
@@ -20,55 +20,24 @@ gulp.task('livereload', function (){
     .pipe(connect.reload());
 });
 
-const colors = new Map([
-  ["white", "#fff"],
-  ["black", "#000"],
-  ["textfield-container", "#f2f2f2"],
-  ["textfield-hover", "#ebebeb"],
-  ["bg-white", "#eee"],
-  ["blue", "#81b3d2"],
-  ["blue-hover", "#669ec0"],
-  ["blue-active", "#5496bd"],
-  ["brown", "#766357"],
-  ["brown-hover", "#604e43"],
-  ["brown-active", "#503e33"],
-  ["footer-white", "#f9fbf6"],
-  ["gray", "#333"],
-  ["shadow-cont", "#000101"],
-  ["nav-current", "#766357"]
-]);
-
-
-
-//не работают кастомные функции, завести или забить...
-gulp.task('scss', () => {
+gulp.task('sass', () => {
   const postcss = require('gulp-postcss');
   const sourcemaps = require('gulp-sourcemaps');
   const postcssImport = require('postcss-import');
   const precss = require('precss');
   const autoprefixer = require('autoprefixer');
   const cssnano = require('cssnano');
-  const functions = require('postcss-functions');
   return gulp
-    .src('./sass/**/*.scss')
+    .src('./sass/main.scss')
     .pipe(sourcemaps.init())
     .pipe(postcss([
       postcssImport(),
       precss(),
-      functions({
-        functions: {
-          color: function(key) {
-            if (){
-              return map-get($colors, $key);
-            }
-          }
-        }
-      }),
       autoprefixer({browsers: ['last 2 versions', 'IE >= 11', '> 1%']}),
       cssnano()
     ]))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('static'));
+    .pipe(gulp.dest('static/css'));
 });
 
-gulp.task('default', ['connect', 'watch', 'scss']);
+gulp.task('default', gulp.series('connect', 'watch', 'sass'));
