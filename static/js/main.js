4592e9285537e3b5922ad9c8b2929e1bb6d41cda
slayer.rus@gmail.com <7^0!CFc*o98ZD>
2018-10-11 16:00:05 +0300

    gulp

--- static/js/main.js
+++ static/js/main.js
@@ -1,32 +1,31 @@
-// Slider range
-function getVals() {
-  // Get slider values
-  let parent = this.parentNode;
-  let slides = parent.getElementsByTagName("input");
-  let slide1 = parseFloat( slides[0].value );
-  let slide2 = parseFloat( slides[1].value );
-  // Neither slider will clip the other, so make sure we determine which is larger
-  if ( slide1 > slide2 ) {
-    let tmp = slide2; slide2 = slide1; slide1 = tmp;
-  }
-  let displayElement = parent.getElementsByClassName("rangeValues")[0];
-  displayElement.innerHTML = slide1 + " - " + slide2;
+let sliderLeft=document.getElementById("min-range");
+let sliderRight=document.getElementById("max-range");
+let inputMin=document.getElementById("min-cost");
+let inputMax=document.getElementById("max-cost");
+
+///value update from input to slider
+//function input update to slider
+function sliderLeftInput(){ //input update slider left
+  sliderLeft.value=inputMin.value;
+}
+function sliderRightInput(){ //input update slider right
+  sliderRight.value=(inputMax.value); //change in input max updated in slider right
 }
 
-window.onload = function() {
-  // Initialize Sliders
-  let sliderSections = document.getElementsByClassName("range-slider");
-  for (let x = 0; x < sliderSections.length; x++ ) {
-    let sliders = sliderSections[x].getElementsByTagName("input");
-    for (let y = 0; y < sliders.length; y++ ){
-      if( sliders[y].type ==="range" ){
-        sliders[y].oninput = getVals;
-        // Manually trigger event first time to display values
-        sliders[y].oninput();
-      }
-    }
-  }
-};
+//calling function on change of inputs to update in slider
+inputMin.addEventListener("change",sliderLeftInput);
+inputMax.addEventListener("change",sliderRightInput);
+
+///value update from slider to input
+//functions to update from slider to inputs
+function inputMinSliderLeft(){ //slider update inputs
+  inputMin.value=sliderLeft.value;
+}
+function inputMaxSliderRight(){//slider update inputs
+  inputMax.value=sliderRight.value;
+}
+sliderLeft.addEventListener("change",inputMinSliderLeft);
+sliderRight.addEventListener("change",inputMaxSliderRight);
 // -------------------------------------------------------
 // Get the modal
 const modal = document.getElementById('appointment-modal');
