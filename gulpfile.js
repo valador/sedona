78619a9054ba3aec48291285715bf59a19dec8f2
slayer.rus@gmail.com <7^0!CFc*o98ZD>
2018-10-11 11:14:03 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const connect = require('gulp-connect');
+const rename = require('gulp-rename');
 
 
 
@@ -29,61 +30,45 @@ gulp.task('livereload', function (done){
 gulp.task('sass', (done) => {
   const postcss = require('gulp-postcss');
   const sourcemaps = require('gulp-sourcemaps');
-//  const postcssImport = require('postcss-import');
+//  const gSass = require('gulp-sass');
+  const postcssImport = require('postcss-import');
   const precss = require('precss');
   const autoprefixer = require('autoprefixer');
   const cssnano = require('cssnano');
-//    const advVariables = require('postcss-advanced-variables');
+//  const advVariables = require('postcss-advanced-variables');
 //  const custProps = require('postcss-custom-properties');
- // const partImport = requiare('postcss-partial-import')
-    const easyImport = require('postcss-easy-import');
+//  const partImport = require('postcss-partial-import');
+//  const easyImport = require('postcss-easy-import');
 //  const nested = require('postcss-nested');
 //  const simpleVars = require('postcss-simple-vars');
+//  const pmix = require('postcss-mixins');
+//  const partImport = require('postcss-partial-import');
 //  const size = require('gulp-size'),
   const sourceFile = 'sass/*.scss';
   const outputFolder = 'static/css';
   return gulp
     .src(sourceFile)
     .pipe(sourcemaps.init())
-    .pipe(postcss([
-//      postcssImport(),
-//      easyImport(),
-      precss(),
-      autoprefixer({browsers: ['last 2 versions', 'IE >= 11', '> 1%']}),
-      cssnano()
-    ]))
-    .pipe(sourcemaps.write('.'))
-//    .pipe(size({ showFiles:true }))
-    .pipe(gulp.dest(outputFolder))
-    .on('end', done);
-});
-
-/*gulp.task('sass', function (done) {
-  const postcss = require('gulp-postcss');
-  const sourcemaps = require('gulp-sourcemaps');
-  const gSass = require('gulp-sass');
-  const autoprefixer = require('autoprefixer');
-  const cssnano = require('cssnano');
-  const SourceFile = 'sass/!*.scss';
-  const outputFolder = 'static/css';
-  return gulp
-    .src(SourceFile)
-    .pipe(sourcemaps.init())
-    .pipe(gSass({
+/*    .pipe(gSass({
       indentType: 'tab',
       indentWidth: 1,
       outputStyle: 'expanded' // Expanded so that our CSS is readable
-    }).on("error", gSass.logError))
+    }).on("error", gSass.logError))*/
     .pipe(postcss([
+//      glob(),
+      postcssImport(),
+      precss(),
       autoprefixer({
         browsers: ['last 2 versions', 'IE >= 11', '> 1%'],
         cascade: false
       }),
       cssnano()
     ]))
+    .pipe(rename('main.css'))
     .pipe(sourcemaps.write('.'))
+//    .pipe(size({ showFiles:true }))
     .pipe(gulp.dest(outputFolder))
     .on('end', done);
-});*/
+});
 
 gulp.task('default', gulp.series('connect', 'watch', 'sass'));
